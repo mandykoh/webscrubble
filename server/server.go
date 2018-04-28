ba52630e30238796fd4e36e2179c1bc31cdbfffd
Amanda Koh <amanda@naucera.net>
2018-04-28 18:26:23 +1000

    Allow server port to be specified.

--- server/server.go
+++ server/server.go
@@ -7,12 +7,23 @@ import (
 	"github.com/go-chi/chi"
 	"github.com/mandykoh/webscrubble/api"
 	"log"
+	"os"
+	"strconv"
 )
 
 func main() {
 	const FrontendRoot = "frontend"
 
 	port := 8080
+	if len(os.Args) > 1 {
+		parsedPort, err := strconv.ParseInt(os.Args[1], 10, 16)
+		if err != nil {
+			log.Fatalf("Unable to parse port '%s'", os.Args[1])
+			os.Exit(1)
+		}
+
+		port = int(parsedPort)
+	}
 
 	endpoints := api.Endpoints{}
 	fileServer := http.FileServer(http.Dir(FrontendRoot))
@@ -27,6 +38,8 @@ func main() {
 		ReadTimeout: 10 * time.Second,
 		WriteTimeout: 10 * time.Second,
 	}
+
+	log.Printf("Starting server on port %d", port)
 
 	log.Fatal(server.ListenAndServe())
 }
